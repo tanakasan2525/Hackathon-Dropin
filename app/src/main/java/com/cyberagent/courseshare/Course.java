ab60627fb8e2e35eaed803325bb48111b1649cf3
nametake <nametake.kyarabuki@gmail.com>
2014-09-10 15:49:20 +0900

    コースクラスに名前と座標リストを追加

--- app/src/main/java/com/cyberagent/courseshare/Course.java
+++ app/src/main/java/com/cyberagent/courseshare/Course.java
@@ -1,9 +1,21 @@
 package com.cyberagent.courseshare;
 
+import java.util.ArrayList;
+
 /**
  * Created by shogo on 2014/09/10.
  */
 public class Course {
+    private String name;
+    private ArrayList<Coordinates> coordinatesList;
 
+    public Course() {
+        coordinatesList = new ArrayList<Coordinates>();
+    }
+
+    public void addCoordinatesToList(double latitude, double longitude) {
+        Coordinates coordinates = new Coordinates(latitude, longitude);
+        coordinatesList.add(coordinates);
+    }
 
 }
