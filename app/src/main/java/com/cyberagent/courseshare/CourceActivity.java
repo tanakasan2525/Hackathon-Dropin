e298835ed876c9c6b6861c014af8d00186374667
tanakasan2525 <tatsuyars@yahoo.co.jp>
2014-09-12 02:32:08 +0900

    Courseeの変更、コース画面用のレイアウトの追加、中間発表用のテスト

--- app/src/main/java/com/cyberagent/courseshare/CourceActivity.java
+++ app/src/main/java/com/cyberagent/courseshare/CourceActivity.java
@@ -1,10 +1,15 @@
 package com.cyberagent.courseshare;
 
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,18 +35,66 @@ public class CourceActivity extends FragmentActivity {
 
 	private Course course;
 
-	private Marker marker;
+	private ArrayList<Marker> markerList;
+
+	LayoutInflater layoutFactory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_cource);
 
+		this.markerList = new ArrayList<Marker>();
+
+		layoutFactory = LayoutInflater.from(this);
 		// テスト用ダミーデータ
-		course = new Course();
+		course = new Course("ダミーコース");
 		course.addCoordinatesToList(35.65787,139.698066);  // 渋谷マークシティ
-		course.addCoordinatesToList(35.66147, 139.709464); // 青山学院大学
+		course.addCoordinatesToList(35.6587292,139.7000174);
+		course.addCoordinatesToList(35.6588081,139.7005457);
+		course.addCoordinatesToList(35.6585071,139.7010799);
+		course.addCoordinatesToList(35.6585152,139.7013252);
+		course.addCoordinatesToList(35.658517, 139.701334);   // 渋谷駅
+		course.addCoordinatesToList(35.6586415, 139.701594);
+		course.addCoordinatesToList(35.6591188, 139.7037352); // 渋谷ヒカリエ
+		course.addCoordinatesToList(35.6590627, 139.7041079);
+		course.addCoordinatesToList(35.6593868, 139.7051022);
+		course.addCoordinatesToList(35.6599376, 139.7055483);
+		course.addCoordinatesToList(35.6600695, 139.7057102);
+		course.addCoordinatesToList(35.6603081, 139.7057727);
+		course.addCoordinatesToList(35.6607825, 139.7066618);
+		course.addCoordinatesToList(35.6613866, 139.7075792);
+		course.addCoordinatesToList(35.6615811, 139.7078944);
+		course.addCoordinatesToList(35.6613542, 139.7082438);
+		course.addCoordinatesToList(35.6619168, 139.7090568); //　青山学院大学
+
+		course.addSpot(new Spot("渋谷マークシティ", new LatLng(35.65787,139.698066),
+				"サイバーエージェントのオフィスがあります。"));
+		course.addSpot(new Spot("渋谷駅", new LatLng(35.658517,139.701334),
+				"JR東日本、東急、東京メトロの駅です。"));
+		course.addSpot(new Spot("渋谷ヒカリエ", new LatLng(35.6591188,139.7037352),
+				"レストラン街、劇場、シェアオフィス、一般オフィスなどがあります。"));
+		course.addSpot(new Spot("青山学院大学", new LatLng(35.66147,139.709464),
+				"青山学院大学の文系が集まるキャンパスです。"));
+
+
+
+		CoursePagerAdapter adapter = new CoursePagerAdapter(this);
+
+		LinearLayout mapView = (LinearLayout)layoutFactory.inflate(R.layout.activity_cource, null);
+		adapter.add(new CourseGuideSpotView(this, this.course));
+		adapter.add(mapView);
+
+
+		// ViewPager を生成
+		ViewPager viewPager = new ViewPager(this);
+		viewPager.setAdapter(adapter);
+
+		// レイアウトにセット
+		setContentView(viewPager);
+
+
+        //setContentView(R.layout.activity_cource);
 
 		setUpMapIfNeeded();
     }
@@ -111,24 +164,34 @@ public class CourceActivity extends FragmentActivity {
 
 		this.map.setInfoWindowAdapter(new CustomInfoAdapter());
 
-		// マーカーの設定と追加
-		addMarker(center, "渋谷マークシティ", "サイバーエージェントのオフィスがあります。");
+		// 主要な場所の案内リストの作成
+		//LinearLayout mainList = (LinearLayout)findViewById(R.id.course_main_list);
+
+		// 主要な場所の処理
+		for (Spot spot : this.course.getSpotList()) {
+			addMarker(spot);
+
+			/*LinearLayout spotView = (LinearLayout)layoutFactory.inflate(R.layout.course_guide_spot, null);
+			TextView text = (TextView)spotView.findViewById(R.id.course_guid_spot_name);
+			text.setText(spot.getName());
+			ImageView thumb = (ImageView)spotView.findViewById(R.id.course_guide_spot_thumb);
+			thumb.setImageResource(R.drawable.thumbnail);
+			mainList.addView(spotView);*/
+		}
     }
 
 	/**
 	 * マーカーを追加します。
-	 * @param latlng
-	 * @param title
-	 * @param desc
+	 * @param spot
 	 */
-	private void addMarker(LatLng latlng, String title, String desc) {
+	private void addMarker(Spot spot) {
 		BitmapDescriptor icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE);
 		MarkerOptions options = new MarkerOptions()
-			.position(latlng)
-			.title(title)
-			.snippet(desc)
+			.position(spot.getCoordinates())
+			.title(spot.getName())
+			.snippet(spot.getDescription())
 			.icon(icon);
-		this.marker = this.map.addMarker(options);
+		this.markerList.add(this.map.addMarker(options));
 	}
 
 	/**
@@ -160,13 +223,25 @@ public class CourceActivity extends FragmentActivity {
 		 */
 		private void render(Marker marker, View view) {
 			// ここでどの Marker がタップされたか判別する
-			if (marker.equals(marker)) {
+			//if (marker.equals(marker)) {
+			// for presentation
+				int imgID = 0;
+				int markerNo = markerList.indexOf(marker);
+				if (markerNo == 0) {
+					imgID = R.drawable.thumbnail;
+				} else if (markerNo == 1) {
+					imgID = R.drawable.shibuyaeki;
+				} else if (markerNo == 2) {
+					imgID = R.drawable.shibuyahikarie;
+				} else if (markerNo == 3) {
+					imgID = R.drawable.aogaku;
+				}
 				// 画像
 				ImageView icon = (ImageView) view.findViewById(R.id.icon);
-				icon.setImageResource(R.drawable.thumbnail);
+				icon.setImageResource(imgID);
 				icon.setMaxWidth(200);
 				icon.setMaxHeight(200);
-			}
+			//}
 			TextView title = (TextView) view.findViewById(R.id.title);
 			TextView snippet = (TextView) view.findViewById(R.id.snippet);
 			title.setText(marker.getTitle());
