ce4ea241f66f91e5181b2df0c66370b2ca066ed9
Akiyuki Kuwabara <kuwa.akiyukimaru@gmail.com>
2014-09-14 15:52:21 +0900

    現在地と向きを取得するPerosonTrackerクラスを追加

---
+++ app/src/main/java/com/cyberagent/courseshare/PersonTracker.java
@@ -0,0 +1,249 @@
+
+package com.cyberagent.courseshare;
+
+import android.app.AlertDialog;
+import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.Gravity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
+
+public class PersonTracker extends Service implements LocationListener, SensorEventListener {
+
+
+    private final Context mContext;
+
+    // flag for GPS status
+    boolean isGPSEnabled = false;
+
+    // flag for network status
+    boolean isNetworkEnabled = false;
+
+    // flag for GPS status
+    boolean canGetLocation = false;
+
+    Location location; // location
+    double latitude; // latitude
+    double longitude; // longitude
+    double orientation; //向き
+
+    // 最小更新距離 Updates in meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 1; // 1 meters
+
+    // 最小更新時間 in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 10 * 1; // 10 seconds
+
+    // LocationManagerとSensorManagerの宣言
+    protected LocationManager locationManager;
+    protected SensorManager mSensorManager;
+
+    public PersonTracker(Context context) {
+        this.mContext = context;
+        // センサーリスナーに登録する
+        registerSensor();
+        //ネットワークの状態を見てLocationリスナーに登録する
+        getLocation();
+    }
+
+    public void registerSensor() {
+        mSensorManager = (SensorManager) mContext.getSystemService(mContext.SENSOR_SERVICE);
+        List<Sensor> sensors = mSensorManager.getSensorList(Sensor.TYPE_ORIENTATION);
+        if (sensors.size() > 0) {
+            Sensor sensor = sensors.get(0);
+            mSensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_UI);
+        }
+    }
+
+    public Location getLocation() {
+        try {
+            locationManager = (LocationManager) mContext
+                    .getSystemService(LOCATION_SERVICE);
+
+            // getting GPS status
+            isGPSEnabled = locationManager
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            // getting network status
+            isNetworkEnabled = locationManager
+                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                // no network provider is enabled
+            } else {
+                this.canGetLocation = true;
+                // First get location from Network Provider
+                if (isNetworkEnabled) {
+                    locationManager.requestLocationUpdates(
+                            LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                    Log.d("Network", "Network");
+                    if (locationManager != null) {
+                        location = locationManager
+                                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if (location != null) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                        }
+                    }
+                }
+                // if GPS Enabled get lat/long using GPS Services
+                if (isGPSEnabled) {
+                    if (location == null) {
+                        locationManager.requestLocationUpdates(
+                                LocationManager.GPS_PROVIDER,
+                                MIN_TIME_BW_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                        Log.d("GPS Enabled", "GPS Enabled");
+                        if (locationManager != null) {
+                            location = locationManager
+                                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (location != null) {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                            }
+                        }
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return location;
+    }
+
+     //GPSの使用をやめる
+     public void stopUsingGPS(){
+        if(locationManager != null){
+            locationManager.removeUpdates(PersonTracker.this);
+            Log.v("Stop", "StopGPS");
+        }
+    }
+
+    public double getLatitude(){
+        if(location != null){
+            latitude = location.getLatitude();
+        }
+        return latitude;
+    }
+
+    public double getLongitude(){
+        if(location != null){
+            longitude = location.getLongitude();
+        }
+
+        return longitude;
+    }
+
+    //GPSかwifiが使えるかのチェック
+    public boolean canGetLocation() {
+        return this.canGetLocation;
+    }
+
+    //GPSの設定を促すダイアログ
+    public void showSettingsAlert(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+
+        // Setting Dialog Title
+        alertDialog.setTitle("GPS is settings");
+
+        // Setting Dialog Message
+        alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
+
+        // On pressing Settings button
+        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog,int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                mContext.startActivity(intent);
+            }
+        });
+
+        // on pressing cancel button
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        // Showing Alert Message
+        alertDialog.show();
+    }
+
+    //人の位置が変わった時の処理
+    @Override
+    public void onLocationChanged(Location location) {
+        this.latitude = location.getLatitude();
+        this.longitude = location.getLongitude();
+
+        //新しい位置に人を表示させるメソッド
+
+    }
+
+
+    @Override
+    public void onProviderDisabled(String provider) {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+    //人の向きが変わった時の処理
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ORIENTATION) {
+            // 方位から回転角を設定
+            float[] values = event.values;
+            this.orientation = values[0];
+//            Log.v("Orientation", String.valueOf(orientation));
+
+            //人の向きに応じて地図を回転させるメソッド
+
+        }
+    }
+
+    public double getOrientation() {
+        return orientation;
+    }
+
+    //Sensorの使用をやめる
+    public void stopUsingSensorOrientation(){
+        if(mSensorManager != null){
+            mSensorManager.unregisterListener(this);
+//            Toast.makeText(this.mContext, "Stop orientation", Toast.LENGTH_LONG).show();
+            Log.v("Stop", "StopOrientation");
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
