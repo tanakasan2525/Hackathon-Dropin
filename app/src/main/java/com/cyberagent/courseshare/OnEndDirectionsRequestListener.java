5c9cdada1a92b03be915b854d643718773191ec9
nametake <nametake.kyarabuki@gmail.com>
2014-09-15 14:39:27 +0900

    Directionリスナーの引数を変更

--- app/src/main/java/com/cyberagent/courseshare/OnEndDirectionsRequestListener.java
+++ app/src/main/java/com/cyberagent/courseshare/OnEndDirectionsRequestListener.java
@@ -3,10 +3,11 @@ package com.cyberagent.courseshare;
 import com.google.android.gms.maps.model.LatLng;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by shogo on 2014/09/15.
  */
 public interface OnEndDirectionsRequestListener {
-    abstract public void onEndDirectionListener(ArrayList<LatLng> latLngs);
+    abstract public void onEndDirectionListener(ArrayList<LatLng> latLngs, HashMap<String, Object> data);
 }
